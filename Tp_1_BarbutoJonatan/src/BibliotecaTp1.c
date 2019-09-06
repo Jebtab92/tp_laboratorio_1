d67b08f1850b3532fcb02b22b6892436c9606114
jebtab92 <jonatanbarbuto@gmail.com>
2019-09-06 20:40:04 -0300

    Final

--- Tp_1_BarbutoJonatan/src/BibliotecaTp1.c
+++ Tp_1_BarbutoJonatan/src/BibliotecaTp1.c
@@ -8,25 +8,25 @@
 #include <stdio.h>
 
 
-int sumar(float pNumeroA, float pNumeroB,float *pSuma)
+int sumar(int pNumeroA, int pNumeroB,int *pSuma)
 {
 	*pSuma = pNumeroA+pNumeroB;
 	return 0;
 }
 
-int restar(float pNumeroA,float pNumeroB,float *pResta)
+int restar(int pNumeroA,int pNumeroB,int *pResta)
 {
 	*pResta = pNumeroA-pNumeroB;
 	return 0;
 }
 
-int multiplicar(float pNumeroA,float pNumeroB,float *pMultiplicacion)
+int multiplicar(int pNumeroA,int pNumeroB,int *pMultiplicacion)
 {
 	*pMultiplicacion = pNumeroA*pNumeroB;
 	return 0;
 }
 
-int dividir(float pNumeroA,float pNumeroB,float *pDivision)
+int dividir(int pNumeroA,int pNumeroB,float *pDivision)
 {
 	if(pNumeroB==0)
 	{
@@ -38,147 +38,155 @@ int dividir(float pNumeroA,float pNumeroB,float *pDivision)
 
 }
 
-int factorizar(int pNumeroA, int pNumeroB)
+int factorizar(int pNumeroA, int pNumeroB,int *pFactorialA, int *pFactorialB)
 {
-	int i;
-	int pFactorialA;
-	int pFactorialB;
+	int i=1;
 	int aFact=1;
 	int bFact=1;
 
-	//Factorial del numero A
-	if(pFactorialA<0)
+	if(pNumeroA<0)
 	{
-		printf("El factorial del numero A es 0\n");
+		*pFactorialA=0;
 	}
-	else if(pFactorialA==0)
+	if(pNumeroA==0)
 	{
-		printf("El factorial del numero B es 1\n");
+		*pFactorialA=1;
 	}
 	else
-		for(i=1;i=pFactorialA;i++)
+	{
+		for(i=1;i<=pNumeroA;i++)
 		{
 			aFact=aFact*i;
 		}
-
-	//Factorial del numero B
-	if(pFactorialB<0)
-	{
-		printf("El factorial del numero B es 0\n");
+	*pFactorialA=aFact;
 	}
-	else if(pFactorialB==0)
+	if(pNumeroB<0)
 	{
-		printf("El factorial del numero B es 1\n");
+		*pFactorialB=0;
+	}
+	if(pNumeroB==0)
+	{
+		*pFactorialB=1;
 	}
 	else
-		for(i=1;i=pFactorialB;i++)
+	{
+		for(i=1;i<=pNumeroB;i++)
 		{
 			bFact=bFact*i;
 		}
-
+	*pFactorialB=bFact;
+	}
 	return 0;
 }
 
-int mostrarMenu(int eleccionMenu,float numeroA,float numeroB)
+int mostrarMenu(int *eleccionMenu,int *numeroA,int *numeroB)
 {
 	int flagA=0;
 	int flagB=0;
 	int flag3=0;
-	float resultSuma;
-	float resultResta;
-	float resultMultiplicacion;
+	int resultSuma;
+	int resultResta;
+	int resultMultiplicacion;
 	float resultDivision;
 	int resultFactorialA;
 	int resultFactorialB;
+	int flagMenu=0;
+
 
 	do
 	{
-	printf("1. Ingresar 1er operando (A=x)\n"
-	"2. Ingresar 2do operando (B=y)\n"
-	"3. Calcular todas las operaciones\n"
-	"a) Calcular la suma (A+B)\n"
-	"b) Calcular la resta (A-B)\n"
-	"c) Calcular la division (A/B)\n"
-	"d) Calcular la multiplicacion (A*B)\n"
-	"e) Calcular el factorial (A!)\n"
-	"4. Informar resultados\n"
-	"a) “El resultado de A+B es: r\n"
-	"b) “El resultado de A-B es: r\n"
-	"c) “El resultado de A/B es: r o “No es posible dividir por cero”\n"
-	"d) “El resultado de A*B es: r\n"
-	"e) “El factorial de A es: r1 y El factorial de B es: r2\n"
-	"5. Salir\n");
-
-	scanf("%d",&eleccionMenu);
-
-	switch(eleccionMenu)
-	{
-	case 1:
-		printf("Ingrese valor de A\n");
-		scanf("%f",&numeroA);
-		flagA=1;
-		break;
-	case 2:
-		printf("Ingrese valor de B\n");
-		scanf("%f",&numeroB);
-		flagB=1;
-		break;
-	case 3:
-		if(flagA==1 && flagB==1)
+		if(flagMenu==0)
 		{
-			sumar(numeroA,numeroB,&resultSuma);
-			restar(numeroA,numeroB,&resultResta);
-			multiplicar(numeroA,numeroB,&resultMultiplicacion);
-			dividir(numeroA,numeroB,&resultDivision);
-			//factorizar(numeroA,numeroB);
+			printf("1. Ingresar 1er operando (A=x)\n"
+				"2. Ingresar 2do operando (B=y)\n"
+				"3. Calcular todas las operaciones\n"
+				"\ta) Calcular la suma (A+B)\n"
+				"\tb) Calcular la resta (A-B)\n"
+				"\tc) Calcular la division (A/B)\n"
+				"\td) Calcular la multiplicacion (A*B)\n"
+				"\te) Calcular el factorial (A!)\n"
+				"4. Informar resultados\n"
+				"\ta) El resultado de A+B es: r\n"
+				"\tb) El resultado de A-B es: r\n"
+				"\tc) El resultado de A/B es: r\n"
+				"\td) El resultado de A*B es: r\n"
+				"\te) El factorial de A es: r1 y El factorial de B es: r2\n"
+				"5. Salir\n");
 
-			flag3=1;
+			scanf("\n%d",&*eleccionMenu);
 		}
-		else
+		if(flagMenu==1)
 		{
-			printf("Falta ingresar un valor\n");
-		}
-		break;
-	case 4:
-		if(flag3==1)
-		{
+			printf("1. Ingresar 1er operando (A=%d)\n"
+					"2. Ingresar 2do operando (B=%d)\n"
+					"3. Calcular todas las operaciones\n"
+					"\ta) Calcular la suma (A+B)\n"
+					"\tb) Calcular la resta (A-B)\n"
+					"\tc) Calcular la division (A/B)\n"
+					"\td) Calcular la multiplicacion (A*B)\n"
+					"\te) Calcular el factorial (A!)\n"
+					"4. Informar resultados\n"
+					"\ta) El resultado de A+B es: %d\n"
+					"\tb) El resultado de A-B es: %d\n",*numeroA,*numeroB,resultSuma,resultResta);
 
-		printf(
-			"1. Ingresar 1er operando (A=%f)\n",numeroA,
-			"2. Ingresar 2do operando (B=%f)\n",numeroB,
-			"3. Calcular todas las operaciones\n"
-			"a) Calcular la suma (A+B)\n"
-			"b) Calcular la resta (A-B)\n"
-			"c) Calcular la division (A/B)\n"
-			"d) Calcular la multiplicacion (A*B)\n"
-			"e) Calcular el factorial (A!)\n"
-			"4. Informar resultados\n"
-			"a) “El resultado de A+B es: %f\n“",resultSuma,
-			"b) “El resultado de A-B es: %f\n“",resultResta);
-
-			if(numeroB !=0)
+			if(*numeroB !=0)
 			{
-				printf("c) “El resultado de A/B es: %f\n“",resultDivision);
+				printf("\tc) El resultado de A/B es: %.2f\n",resultDivision);
 			}
 			else
 			{
-				printf("“No es posible dividir por cero”\n");
+				printf("\tc)No es posible dividir por cero\n");
 			}
+			printf("\td) El resultado de A*B es: %d\n"
+				   "\te) El factorial de A es: %d y El factorial de B es: %d\n"
+				   "5. Salir\n",resultMultiplicacion,resultFactorialA,resultFactorialB);
 
-		printf("“El resultado de A*B es: %f\n“",resultMultiplicacion,
-			"e) “El factorial de A es: %d y El factorial de B es: %d\n“"
-				,resultFactorialA,resultFactorialB,
-			"5. Salir\n");
+			scanf("\n%d",&*eleccionMenu);
 		}
-		else
+
+
+		switch(*eleccionMenu)
 		{
-			printf("Falta realizar los calculos\n");
-		}
-		break;
-	case 5:
-		break;
-	}
-	}while(eleccionMenu != 5);
-	return 0;
+			case 1:
+				printf("Ingrese valor de A\n");
+				scanf("\n%d",&*numeroA);
+				flagA=1;
+				break;
+			case 2:
+				printf("Ingrese valor de B\n");
+				scanf("\n%d",&*numeroB);
+				flagB=1;
+				break;
+			case 3:
+				if(flagA==1 && flagB==1)
+				{
+					sumar(*numeroA,*numeroB,&resultSuma);
+					restar(*numeroA,*numeroB,&resultResta);
+					multiplicar(*numeroA,*numeroB,&resultMultiplicacion);
+					dividir(*numeroA,*numeroB,&resultDivision);
+					factorizar(*numeroA,*numeroB,&resultFactorialA,&resultFactorialB);
+					flag3=1;
+				}
+				else
+				{
+					printf("Falta ingresar un valor\n");
+				}
+				flag3=1;
+				break;
+			case 4:
+				if(flag3==1)
+				{
+					flagMenu=1;
+				}
+				else
+				{
+					printf("Falta realizar los calculos\n");
+				}
 
+				break;
+			case 5:
+				break;
+			}
+	}while(*eleccionMenu != 5);
+	return 0;
 }
